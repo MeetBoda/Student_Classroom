634ec19373f34bbfc5a3ba92ad9b7a2cbfe00693
MeetBoda <96529606+MeetBoda@users.noreply.github.com>
2023-03-14 22:35:07 +0530

    Delete student_classroom directory

--- student_classroom/classrooms/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from classrooms.models import Classrooms
-
-class ClassroomsAdmin(admin.ModelAdmin):
-    list_display=('classroom_name',)
-
-admin.site.register(Classrooms, ClassroomsAdmin)
-
-# Register your models here.
--- student_classroom/users/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from users.models import Users
-
-class UsersAdmin(admin.ModelAdmin):
-    list_display=('user_id','name', 'email', 'role')
-
-admin.site.register(Users, UsersAdmin)
-
-# Register your models here.
