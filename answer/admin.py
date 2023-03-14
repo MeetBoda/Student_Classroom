04206e30bc49e9ec247057f5f74f66d2f44b18ec
MeetBoda <96529606+MeetBoda@users.noreply.github.com>
2023-03-14 22:54:59 +0530

    Delete answer directory

--- answer/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from answer.models import Answers
-
-# Register your models here.
-class AnswerAdmin(admin.ModelAdmin):
-    list_display=('user_id', 'ans', 'classroom_id', 'doubt_id')
-
-admin.site.register(Answers, AnswerAdmin)
